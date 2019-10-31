cc9b297a60f653bab63940ed89046cb85fdb0e4c
Jeremy Rose <jeremy.r.rose@gmail.com>
2019-10-31 11:27:01 -0400

    hamburger menus complete

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let burgerMenu = document.querySelector('.menuframe');
-burgerMenu.style.width = "0px";
+// burgerMenu.style.width = "0px";
 let mainMenu = document.querySelector('.menu');
 let hamburger = document.querySelector('.hamburger');
 let mainBurgerIcon = document.querySelector('.mainBurger');
@@ -7,9 +7,11 @@ let sideBurgerIcon = document.querySelector('.burgerButton');
 let menuBurgerDiv = document.querySelector('.menuBurger');
 let arrowIcon = document.querySelector('.arrow');
 let menuItems = document.querySelector('.menuframe').querySelectorAll('a');
+let logo = document.querySelector('.logo');
+let mobileBurger = logo.querySelector('.mobileBurger');
 
 const opacitizer = (arr, initTimeout = 0, inc = 50) => {
-    if (burgerMenu.style.width != "0px") {
+    if (burgerMenu.classList[1]) {
         for (let i = 0; i < arr.length; i++) {
             setTimeout(() => arr[i].style.opacity = 1, initTimeout);
             initTimeout += inc;
@@ -18,24 +20,42 @@ const opacitizer = (arr, initTimeout = 0, inc = 50) => {
 }
 
 const menuToggle = () => {
-    if (burgerMenu.style.width === "0px") {
-        hamburger.style.opacity = 0;
-        hamburger.style.width = "0px";
-        // mainMenu.style.gridTemplateColumns = "0px repeat(4, 1fr)";
-        sideBurgerIcon.style.opacity = 1;
-        burgerMenu.addEventListener('transitionend',() => opacitizer(menuItems, 0, 40));
-        burgerMenu.style.width = "280px";
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        if (!burgerMenu.classList[1]) {
+            document.body.classList.add('noScroll');
+            logo.classList.add('sticky');
+            burgerMenu.addEventListener('transitionend',() => opacitizer(menuItems, 0, 40));
+            burgerMenu.classList.add('open');
+            mainMenu.classList.add('open');
+        } else {            
+            burgerMenu.classList.remove('open');
+            mainMenu.classList.remove('open');
+            document.body.classList.remove('noScroll');
+            if (window.scrollY < stickyPoint) {
+                window.scrollTo(0, stickyPoint);
+            }
+            menuItems.forEach(item => item.style.opacity = 0);
+        }
     } else {
-        sideBurgerIcon.style.opacity = 0;
-        burgerMenu.style.width = "0px";
-        // mainMenu.style.gridTemplateColumns = "50px repeat(4, 1fr)";
-        hamburger.style.width = "80px";
-        hamburger.style.opacity = 1;
-        menuItems.forEach(item => item.style.opacity = 0);
-        // timeouts.forEach(item => clearTimeout(item));
-        // menuItems.forEach(item => {
-        //     console.log(item, item.style.opacity); item.style.opacity = 0;
-        // });
+        if (!burgerMenu.classList[1]) {
+            hamburger.style.opacity = 0;
+            setTimeout(() => hamburger.style.width = "0px", 600);
+            // mainMenu.style.gridTemplateColumns = "0px repeat(4, 1fr)";
+            sideBurgerIcon.style.opacity = 1;
+            burgerMenu.addEventListener('transitionend',() => opacitizer(menuItems, 0, 40));
+            burgerMenu.classList.add('open');
+        } else {
+            sideBurgerIcon.style.opacity = 0;
+            burgerMenu.classList.remove('open');
+            // mainMenu.style.gridTemplateColumns = "50px repeat(4, 1fr)";
+            hamburger.style.width = "80px";
+            hamburger.style.opacity = 1;
+            menuItems.forEach(item => item.style.opacity = 0);
+            // timeouts.forEach(item => clearTimeout(item));
+            // menuItems.forEach(item => {
+            //     console.log(item, item.style.opacity); item.style.opacity = 0;
+            // });
+        }
     }
 }
 
@@ -53,3 +73,19 @@ hamburger.addEventListener('click',menuToggle);
 hamburger.addEventListener('mouseover',() => mainBurgerIcon.setAttribute('src','images/hamburgerHover.svg'));
 hamburger.addEventListener('mouseout',() => mainBurgerIcon.setAttribute('src','images/hamburger.svg'));
 
+mobileBurger.addEventListener('click',menuToggle);
+mobileBurger.addEventListener('mouseover',() => mobileBurger.setAttribute('src','images/hamburgerHover.svg'));
+mobileBurger.addEventListener('mouseout',() => mobileBurger.setAttribute('src','images/hamburger.svg'));
+
+let stickyPoint = logo.offsetTop;
+const stickinator = () => {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        if (window.pageYOffset >= stickyPoint) {
+            logo.classList.add('sticky');
+        } else {
+            logo.classList.remove('sticky');
+        }
+    }
+}
+
+window.onscroll = stickinator;
